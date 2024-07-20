24085be4afb83e61326d58ea37cbd0d92acdf9d7
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-07-20 21:57:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,27 +42,25 @@ def get_image_from_base64_string(b64str):
 @app.route('/',methods=['GET'])
 def home():
     return "Hello World"
-labels = ['glioma_tumor','meningioma_tumor','no_tumor','pituitary_tumor']
 @app.route("/predict", methods=['POST'])
 def read_root():
-    print("NAAA1")
-    # data = JSON.loads(request.data)
+    labels = ['glioma_tumor','meningioma_tumor','no_tumor','pituitary_tumor']
     data = request.get_json()
     print("NAAA2 loaded")
     predict_img = []
     item = data['image'][0]
-    image=get_cv2_image_from_base64_string(item)
-    image = cv2.resize(image,(150,150))
-    predict_img.append(image)
+    encoded_data = item.split(',')[1]
+    image_data = BytesIO(base64.b64decode(encoded_data))
+    pil_image = Image.open(image_data)
+    resized_image = pil_image.resize((150, 150))
+    predict_img.append(resized_image)
     print("NAAAA3")
     img_array = np.array(predict_img)
     print("NAAAAA4")
-    # img_array2 = img_array.reshape(1,150,150,3)
     res2 = loaded_model.predict(img_array)
-    res3 = res2.argmax()
-    # res2 = 0
-    return {"result":labels[res3]}
+    res2 = res2.argmax()
+    return {"result":labels[res2]}
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=6000)
