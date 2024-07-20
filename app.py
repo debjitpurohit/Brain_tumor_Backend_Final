600511da593ee2a9fb653d5423edbc1f91c22b4c
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-07-20 19:32:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,21 +47,22 @@ labels = ['glioma_tumor','meningioma_tumor','no_tumor','pituitary_tumor']
 def read_root():
     print("NAAA1")
     # data = JSON.loads(request.data)
+    data = request.get_json()
     print("NAAA2 loaded")
-    # predict_img = []
-    # for item in data['image']:
-    #     #Decode the base64-encoded image
-    #     image = get_cv2_image_from_base64_string(item)
-    #     image = cv2.resize(image,(150,150))
-    #     predict_img.append(image)
+    predict_img = []
+    for item in data['image']:
+        #Decode the base64-encoded image
+        image = get_cv2_image_from_base64_string(item)
+        image = cv2.resize(image,(150,150))
+        predict_img.append(image)
 
     print("NAAAA3")
-    # img_array = np.array(predict_img)
+    img_array = np.array(predict_img)
     print("NAAAAA4")
     # img_array = img_array.reshape(1,150,150,3)
-    # res2 = loaded_model.predict(img_array)
-    # res2 = res2.argmax()
-    res2 = 0
+    res2 = loaded_model.predict(img_array)
+    res2 = res2.argmax()
+    # res2 = 0
     return {"result":labels[res2]}
 
 
